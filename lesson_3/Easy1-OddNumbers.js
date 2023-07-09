5ff17fd076dddcd37b7b6225f3d2926659875abb
Ricardo Landeros <landerosr81@gmail.com>
2023-07-08 21:46:44 -0700

    Odd Numbers

--- lesson_3/Easy1-OddNumbers.js
+++ lesson_3/Easy1-OddNumbers.js
@@ -1,3 +1,8 @@
+/*
+Log all odd numbers from 1 to 99, inclusive, to the console, with each number
+on a separate line.
+*/
+
 for (let number = 1; number <= 99; number++) {
   if (number % 2 === 1) {
     console.log(number);
