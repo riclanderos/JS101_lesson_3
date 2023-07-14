297c42f0034c4d8aa3882fba68f6e9f710ea7c2d
Ricardo Landeros <landerosr81@gmail.com>
2023-07-13 22:01:42 -0700

    Practice Problems: Easy 3

--- lesson_3/practiceProblemsEasy3.js
+++ lesson_3/practiceProblemsEasy3.js
@@ -1,10 +1,16 @@
-// Question 1. Write three different ways to remove all of the elements from the following array:
+// Question 1. Write three different ways to remove all of the elements
+// from the following array:
 let numbers = [1, 2, 3, 4];
-//=> numbers.splice(0, numbers.length);
-//=> numbers.length = 0;
-/*=> while (numbers.length) {
+
+// Answers:
+/* option 1 */
+numbers.splice(0, numbers.length);
+/* option 2 */
+numbers.length = 0;
+/* option 3 */
+while (numbers.length) {
   numbers.pop();
-} */
+}
 
 //Question 2. What will the following code output?
 console.log([1, 2, 3] + [4, 5]); //=> It will concatenate the code and return 1,2,34,5
@@ -16,13 +22,17 @@ str2 = "goodbye!";
 console.log(str1); //=> The output will be 'hello there'. The strings are immutable so str1 will continue to hold its original value.
 
 // Question 4. What will the following code output?
-let arr1 = [{ first: "value1" }, { second: "value2" }, 3, 4, 5];
+let arr1 = [{first: "value1"}, {second: "value2"}, 3, 4, 5];
 let arr2 = arr1.slice();
 arr2[0].first = 42;
-console.log(arr1); 
+console.log(arr1);
 //=> [{first: 42}, {second: "value2"}, 3,4,5];
 
-// Question 5. The following function unnecessarily uses two return statements to return boolean values. Can you rewrite this function so it only has one return statement and does not explicitly use either true or false?
+/* Question 5. The following function unnecessarily uses
+two return statements to return boolean values. Can you
+rewrite this function so it only has one return statement
+and does not explicitly use either true or false?
+*/
 function isColorValid(color) {
   if (color === "blue" || color === "green") {
     return true;
@@ -31,8 +41,9 @@ function isColorValid(color) {
   }
 }
 
-/*=> function isColorValid(color) {
-  return color === 'blue' || color === 'green';
+//Answer:
+function isColorValid1(color) {
+  return color === "blue" || color === "green";
 }
 
-=> const isColorValid = color => ['blue', 'green'].includes(color); */
+const isColorValid2 = (color) => ["blue", "green"].includes(color);
